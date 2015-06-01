af3531e3e68984bbb95ad745d18951927cf056de
John Antreassian <janter@gmail.com>
2015-05-31 23:39:26 -0400

    Added middleware to Authentication and Password controllers, which will make sure authenticated users don't get to pages they shouldn't and anonymous users don't get to pages they shouldn't.

---
+++ app/Services/Registrar.php
@@ -0,0 +1,29 @@
+<?php namespace App\Services;
+
+use App\User;
+use Validator;
+use Illuminate\Contracts\Auth\Registrar as RegistrarContract;
+
+class Registrar implements RegistrarContract {
+
+	/**
+	 * Get a validator for an incoming registration request.
+	 *
+	 * @param  array  $data
+	 * @return \Illuminate\Contracts\Validation\Validator
+	 */
+	public function validator(array $data)
+	{
+	}
+
+	/**
+	 * Create a new user instance after a valid registration.
+	 *
+	 * @param  array  $data
+	 * @return User
+	 */
+	public function create(array $data)
+	{
+	}
+
+}
