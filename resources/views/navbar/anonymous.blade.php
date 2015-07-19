891c8bd0c1344da331c3c2e9f7a6a9a00db49819
John Antreassian <janter@gmail.com>
2015-07-19 02:08:30 -0400

    Begining styling.

--- resources/views/navbar/anonymous.blade.php
+++ resources/views/navbar/anonymous.blade.php
@@ -1,8 +1,24 @@
 <nav class="navbar navbar-default" role="navigation">
   <div class="container">
     <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
       <a class="navbar-brand hidden-xs" href="/"><span class='link'>Laravel Application</span></a>
     </div> <!-- navbar-header -->
-        <div class='hidden-xs login-form-wrapper'>@include('forms.login_form')</div>
+      <div id="navbar" class="navbar-collapse collapse" aria-expanded="false" style="height: 1px;">
+
+         <ul class="nav navbar-nav navbar-right">
+            <li>
+                <a href="/login"><button type="button" class="btn btn-default navbar-btn">Sign in</button></a>
+            </li>
+            <li>
+                <a href="/signup"><button type="button" class="btn btn-default navbar-btn">Sign up</button></a>
+            </li>
+        </ul>
+    </div>
   </div><!-- container -->
 </nav>
